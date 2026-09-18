e62141b631647757810ffc9047ccb2338ab31123
Ronald <56053290+Ronald-Luiz@users.noreply.github.com>
2026-09-18 09:10:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -371,12 +371,7 @@ export default function App() {
               
     <button className="btn-novo" id="btnNovoAtendimento" type="button" > ＋ Novo Atendimento </button>
 
-                <button className="menu-item" id="menuNovoAtendimento">
-              <span>
-                ＋
-              </span>
-              Novo Atendimento
-            </button>
+           
 
 
 
